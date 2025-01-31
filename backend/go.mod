3bf3fa18c289710a4509273c369882acbe53dcff
settivishal <vishalsetti2000@gmail.com>
2025-01-31 12:34:54 -0500

    implement login and register apis

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,8 @@ go 1.23.3
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/gorilla/mux v1.8.1
-	github.com/lib/pq v1.10.9
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
+
+require golang.org/x/crypto v0.32.0 // indirect
---
+++ backend/middleware/auth_middleware.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"backend/utils"
+)
+
+func JWTAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			utils.SendResponse(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		token := strings.Split(authHeader, "Bearer ")
+		if len(token) < 2 {
+			utils.SendResponse(w, "Invalid token format", http.StatusUnauthorized)
+			return
+		}
+
+		claims, err := utils.ValidateJWT(token[1])
+		if err != nil {
+			utils.SendResponse(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		// Attach user info to request context
+		ctx := context.WithValue(r.Context(), "username", claims.Username)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
