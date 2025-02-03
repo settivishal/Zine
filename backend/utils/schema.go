ea1b903bdeaea47410244d7b5654387020244959
settivishal <vishalsetti2000@gmail.com>
2025-02-03 15:50:10 -0500

    minor changes

--- backend/utils/schema.go
+++ backend/utils/schema.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Credentials struct {
-	Name 		 string `json:"name"`
+	Name     string `json:"name"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
 }
@@ -18,15 +18,15 @@ type Claims struct {
 }
 
 type RegisterResponse struct {
-	Message  string `json:"message"`
-	Email    string `json:"email"`
-	Name string `json:"name"`
+	Message string `json:"message"`
+	Email   string `json:"email"`
+	Name    string `json:"name"`
 }
 
 type LoginResponse struct {
 	Message      string    `json:"message"`
-	Name     		 string    `json:"name"`
-	Email				 string    `json:"email"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
 	AccessToken  string    `json:"access_token"`
 	RefreshToken string    `json:"refresh_token"`
 	ExpiresAt    time.Time `json:"expires_at"`
