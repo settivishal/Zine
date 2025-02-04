2b3dbc7002e84b0aa258f7e714269a169dc0ff41
settivishal <vishalsetti2000@gmail.com>
2025-02-04 02:32:37 -0500

    implemented update or create functionality #9

--- backend/controllers/social_controller.go
+++ backend/controllers/social_controller.go
@@ -9,8 +9,9 @@ import (
 
 	"backend/config"
 	"backend/db"
-	"backend/models"
 	"backend/services"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // GoogleUser represents the user info obtained from Google
@@ -45,53 +46,56 @@ func GoogleCallback(w http.ResponseWriter, r *http.Request) {
 
 	token, err := config.GoogleOauthConfig.Exchange(context.Background(), code)
 	if err != nil {
-		utils.SendResponse(w, "Failed to exchange token", http.StatusInternalServerError)
+		utils.SendErrorResponse(w, "Failed to exchange token", err, http.StatusInternalServerError)
 		return
 	}
 
 	client := config.GoogleOauthConfig.Client(context.Background(), token)
 	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
 	if err != nil {
-		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		utils.SendErrorResponse(w, "Failed to get user info", err, http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		http.Error(w, "Failed to read user info", http.StatusInternalServerError)
+		utils.SendErrorResponse(w, "Failed to read user info", err, http.StatusInternalServerError)
 		return
 	}
 
-	var google_user GoogleUser
-	err = json.Unmarshal(body, &google_user)
+	var user GoogleUser
+	err = json.Unmarshal(body, &user)
 	if err != nil {
-		http.Error(w, "Failed to unmarshal user info", http.StatusInternalServerError)
+		utils.SendErrorResponse(w, "Failed to unmarshal user info", err, http.StatusInternalServerError)
 		return
 	}
 
-	user := models.User{
-		Email:      google_user.Email,
-		Name:       google_user.Name,
-		Picture:    google_user.Picture,
-		OauthToken: google_user.ID,
+	// Update ot Create User Data
+	updateData := bson.M{
+		"email":       user.Email,
+		"name":        user.Name,
+		"picture":     user.Picture,
+		"oauth_token": user.ID,
 	}
 
-	if err := database.InsertUser(user); err != nil {
+	if err := database.UpsertUser("users", bson.M{"email": user.Email}, updateData); err != nil {
 		utils.SendErrorResponse(w, "Error saving user", err, http.StatusInternalServerError)
 		return
 	}
 
+	// Generate Access and Refresh Tokens
 	credentials := utils.Credentials{
 		Email: user.Email,
 		Name:  user.Name,
 	}
 	accessToken, accessExpiry, refreshToken := services.GenerateTokens(credentials, w)
 
+	// Response structure
 	response := utils.LoginResponse{
 		Message:      "Registration successful",
-		Name:         google_user.Name,
-		Email:        google_user.Email,
+		Name:         user.Name,
+		Email:        user.Email,
 		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
 		ExpiresAt:    accessExpiry,
