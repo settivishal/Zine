4ed936afeccd65153b96914dce59b68a29bc8a16
shivarao18 <shiva.thummanapalli@gmail.com>
2025-02-06 10:52:32 -0500

    CORS resolved

--- backend/main.go
+++ backend/main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
 	"backend/config"
 	database "backend/db"
 	"backend/routes"
 
-    "github.com/gorilla/mux"
-    "github.com/rs/cors" // Added import for CORS
+	"github.com/gorilla/mux"
+	"github.com/rs/cors" // Added import for CORS
 )
 
 func main() {
-    // Load configuration file
-    if err := config.LoadConfig(); err != nil {
-        log.Fatal("Failed to load config:", err)
-    }
+	// Load configuration file
+	if err := config.LoadConfig(); err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
 
-    // Connect to the MongoDB
-    database.ConnectDB()
-    defer database.DisconnectDB()
+	// Connect to the MongoDB
+	database.ConnectDB()
+	defer database.DisconnectDB()
 
-    // Initialize the router
-    router := mux.NewRouter()
+	// Initialize the router
+	router := mux.NewRouter()
 
-    // Register routes
-    routes.Routes(router) // Using the existing Routes function
+	// Register routes
+	routes.Routes(router) // Using the existing Routes function
 
-    // Set up CORS
-    cors.New(cors.Options{
-        AllowedOrigins: []string{"http://localhost:3000"}, // Allow access from localhost:3000
-        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowedHeaders: []string{"Content-Type", "Authorization"},
-	
-})
+	// Set up CORS
+	cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"}, // Allow access from localhost:3000
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+	})
 
-    log.Println("Swagger UI available at http://localhost:" + "8080" + "/swagger/index.html")
-	log.Fatal(http.ListenAndServe(":"+"8080", router))
-
+	port := config.Env("PORT", "8080")
+	log.Println("Server running on port", port, "...")
+	log.Println("Swagger UI available at http://localhost:" + port + "/swagger/index.html")
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
