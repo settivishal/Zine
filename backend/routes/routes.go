a1063b5228e0de300fa2dfad2fc97fcc14b7cf3e
Jaiharishan <jaiharishanav@gmail.com>
2025-03-01 16:40:50 -0500

    route changes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -29,7 +29,7 @@ func Routes(router *mux.Router) {
 	api.Use(middleware.JWTAuthMiddleware)
 	api.HandleFunc("/profile", controllers.GetProfile).Methods("GET")
 	api.HandleFunc("/change_password", controllers.ChangePassword).Methods("POST")
-	api.HandleFunc("/tags", controllers.CreateTag).Methods("POST")
+	api.HandleFunc("/tag/create", controllers.CreateTag).Methods("POST")
 }
 
 func SwaggerRoutes(router *mux.Router) {
