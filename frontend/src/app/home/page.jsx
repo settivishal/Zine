49c5acd65123adb6a5a0550a8cc0aa868504ca7d
shivarao18 <shiva.thummanapalli@gmail.com>
2025-03-24 14:48:55 -0400

    add tags with mock data

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -1,43 +1,38 @@
-
 'use client';
 import { useState } from 'react';
-import CalendarComponent from '../../components/Calendar';
-import CalendarWidget from '../../components/CalendarWidget';
 import Navbar from '../../components/Navbar';
-import TagsComponent from '../../components/Tags'; // Import the new component
+import CalendarWidget from '../../components/CalendarWidget';
+import BlogList from '../../components/Calendar';
+// import TagsComponent from '../../components/tags'; // Import the new component
 
-export default function CalendarLayout() {
-  const [calendarView, setCalendarView] = useState('dayGridMonth');
+export default function Page() {
+    const [calendarView, setCalendarView] = useState('dayGridMonth');
 
-  const handleViewChange = (newView) => {
-    setCalendarView(newView);
-  };
+    const handleViewChange = (newView) => {
+        setCalendarView(newView);
+    };
 
-  return (
-    <div className="relative flex flex-col p-4 min-h-screen bg-white">
-      {/* Navbar */}
-      <Navbar />
-      
-      {/* Main Content */}
-      <div className="flex flex-row gap-4 h-[calc(100vh-80px)] overflow-y-auto">
-        {/* Sidebar */}
-        <div className="flex-shrink-0 space-y-4">
-          {/* Calendar Widget */}
-          <CalendarWidget />
+    return (
+        <div className="relative flex flex-col p-4 min-h-screen bg-white">
+            {/* Navbar */}
+            <Navbar />
 
-          {/* Tags Component */}
-          <TagsComponent />
+            {/* Main Content */}
+            <div className="flex flex-row gap-4 h-[calc(100vh-80px)] overflow-y-auto">
+                {/* Sidebar */}
+                <div className="flex-shrink-0 space-y-4">
+                    {/* Calendar Widget */}
+                    <CalendarWidget />
+
+                    {/* Tags Component */}
+                    {/* <TagsComponent /> */}
+                </div>
+
+                {/* Blog List */}
+                <div className="flex-grow bg-white rounded-lg shadow-md p-4">
+                    <BlogList />
+                </div>
+            </div>
         </div>
-
-        {/* Full Calendar */}
-        <div className="flex-grow">
-          <CalendarComponent 
-            view={calendarView} 
-            onViewChange={handleViewChange} 
-            key={calendarView} 
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
