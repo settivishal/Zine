b60ccb9149c996afd30c36fa4fd7a756b130cdc5
settivishal <vishalsetti2000@gmail.com>
2025-03-30 16:48:09 -0400

    added unit test for get blog

--- backend/controllers/blog_controller_test.go
+++ backend/controllers/blog_controller_test.go
@@ -1,4 +1,65 @@
 package controllers
 
-import ()
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockHandleGetBlog struct {
+	mock.Mock
+}
+
+func (m *MockHandleGetBlog) MockHandleGetBlog(w http.ResponseWriter, r *http.Request) (interface{}, error, int) {
+	args := m.Called(r)
+	return args.Get(0), args.Error(1), args.Int(2)
+}
+
+func TestGetBlog(t *testing.T) {
+	// Test case 1: tag created successfully
+	t.Run("Successful CreateTag Response", func(t *testing.T) {
+		// Mock dependencies
+		mockHandleGetBlog := new(MockHandleGetBlog)
+
+		req := httptest.NewRequest(http.MethodPost, "/blog/{_id}", nil)
+
+		mockHandleGetBlog.On("MockHandleGetBlog", req).Return("Tag created successfully", nil, http.StatusOK)
+
+		// Call the mock method
+		w := httptest.NewRecorder()
+		response, err, statusCode := mockHandleGetBlog.MockHandleGetBlog(w, req)
+
+		// Assertions
+		assert.Equal(t, "Tag created successfully", response)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, statusCode)
+
+		// Verify that the expected call was made
+		mockHandleGetBlog.AssertExpectations(t)
+	})
+
+	// Test case 2: error creating tag
+	t.Run("Error CreateTag Response", func(t *testing.T) {
+		mockHandleGetBlog := new(MockHandleGetBlog)
+
+		req := httptest.NewRequest(http.MethodPost, "/blog/{_id}", nil)
+
+		mockHandleGetBlog.On("MockHandleGetBlog", req).Return(nil, errors.New("error creating tag"), http.StatusInternalServerError)
+
+		// Call the mock method
+		w := httptest.NewRecorder()
+		response, err, statusCode := mockHandleGetBlog.MockHandleGetBlog(w, req)
+
+		// Assertions
+		assert.Nil(t, response)
+		assert.Equal(t, "error creating tag", err.Error())
+		assert.Equal(t, http.StatusInternalServerError, statusCode)
+
+		// Verify that the expected call was made
+		mockHandleGetBlog.AssertExpectations(t)
+	})
+}
