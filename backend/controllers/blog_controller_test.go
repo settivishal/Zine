f2c9a887a2809fc6c0614e5c0abb7fe1a0ac5648
settivishal <vishalsetti2000@gmail.com>
2025-03-30 21:57:19 -0400

    unit test case for save blog

--- backend/controllers/blog_controller_test.go
+++ backend/controllers/blog_controller_test.go
@@ -18,12 +18,21 @@ type MockHandleCreateBlog struct {
 	mock.Mock
 }
 
+type MockHandleSaveBlog struct {
+	mock.Mock
+}
+
 func (m *MockHandleGetBlog) MockHandleGetBlog(w http.ResponseWriter, r *http.Request) (interface{}, error, int) {
 	args := m.Called(r)
 	return args.Get(0), args.Error(1), args.Int(2)
 }
 
 func (m *MockHandleCreateBlog) MockHandleCreateBlog(w http.ResponseWriter, r *http.Request) (interface{}, error, int) {
+	args := m.Called(r)
+	return args.Get(0), args.Error(1), args.Int(2)
+}
+
+func (m *MockHandleSaveBlog) MockHandleSaveBlog(w http.ResponseWriter, r *http.Request) (interface{}, error, int) {
 	args := m.Called(r)
 	return args.Get(0), args.Error(1), args.Int(2)
 }
@@ -115,5 +124,50 @@ func TestCreateBlog(t *testing.T) {
 
 		// Verify that the expected call was made
 		mockHandleCreateBlog.AssertExpectations(t)
+	})
+}
+
+func TestSaveBlog(t *testing.T) {
+	// Test case 1: tag created successfully
+	t.Run("Successful CreateTag Response", func(t *testing.T) {
+		// Mock dependencies
+		mockHandleSaveBlog := new(MockHandleSaveBlog)
+
+		req := httptest.NewRequest(http.MethodPost, "/blog/save", nil)
+
+		mockHandleSaveBlog.On("MockHandleSaveBlog", req).Return("Tag created successfully", nil, http.StatusOK)
+
+		// Call the mock method
+		w := httptest.NewRecorder()
+		response, err, statusCode := mockHandleSaveBlog.MockHandleSaveBlog(w, req)
+
+		// Assertions
+		assert.Equal(t, "Tag created successfully", response)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, statusCode)
+
+		// Verify that the expected call was made
+		mockHandleSaveBlog.AssertExpectations(t)
+	})
+
+	// Test case 2: error creating tag
+	t.Run("Error CreateTag Response", func(t *testing.T) {
+		mockHandleSaveBlog := new(MockHandleSaveBlog)
+
+		req := httptest.NewRequest(http.MethodPost, "/blog/save", nil)		
+
+		mockHandleSaveBlog.On("MockHandleSaveBlog", req).Return(nil, errors.New("error creating tag"), http.StatusInternalServerError)
+
+		// Call the mock method
+		w := httptest.NewRecorder()
+		response, err, statusCode := mockHandleSaveBlog.MockHandleSaveBlog(w, req)
+
+		// Assertions
+		assert.Nil(t, response)
+		assert.Equal(t, "error creating tag", err.Error())		
+		assert.Equal(t, http.StatusInternalServerError, statusCode)
+
+		// Verify that the expected call was made		
+		mockHandleSaveBlog.AssertExpectations(t)
 	})
 }
