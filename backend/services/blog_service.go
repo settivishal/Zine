a9b60fbfc2d27d4f7a05f9609358fdd43565678d
settivishal <vishalsetti2000@gmail.com>
2025-04-19 13:32:26 -0400

    refactoring

--- backend/services/blog_service.go
+++ backend/services/blog_service.go
@@ -249,9 +249,9 @@ func HandleGetBlogByDate(w http.ResponseWriter, r *http.Request) (*utils.GetBlog
 func HandleGetBlogsByTagIDs(w http.ResponseWriter, r *http.Request) (*utils.GetBlogsResponse, error, int) {
 	email, ok := r.Context().Value("email").(string)
 
-    if !ok {
-        return nil, errors.New("Error getting email"), http.StatusBadRequest
-    }
+	if !ok {
+		return nil, errors.New("Error getting email"), http.StatusBadRequest
+	}
 
 	var payload utils.TagsRequestPayload
 
@@ -261,30 +261,30 @@ func HandleGetBlogsByTagIDs(w http.ResponseWriter, r *http.Request) (*utils.GetB
 	}
 
 	if len(payload.TagIDs) == 0 {
-        return nil, errors.New("At least one tag ID is required"), http.StatusBadRequest
-    }
+		return nil, errors.New("At least one tag ID is required"), http.StatusBadRequest
+	}
 
 	// Get pagination parameters
-    query := r.URL.Query()
-    page, _ := strconv.Atoi(query.Get("page"))
-    if page < 1 {
-        page = 1
-    }
-    limit, _ := strconv.Atoi(query.Get("limit"))
-    if limit < 1 {
-        limit = 10 // Default page size
-    }
+	query := r.URL.Query()
+	page, _ := strconv.Atoi(query.Get("page"))
+	if page < 1 {
+		page = 1
+	}
+	limit, _ := strconv.Atoi(query.Get("limit"))
+	if limit < 1 {
+		limit = 10 // Default page size
+	}
 
 	// Fetch blogs by tag IDs
-    blogs, err, count, totalPages := database.GetBlogsByTagIDs(email, payload.TagIDs, page, limit)
-    if err != nil {
+	blogs, err, count, totalPages := database.GetBlogsByTagIDs(email, payload.TagIDs, page, limit)
+	if err != nil {
 		return nil, errors.New("Error fetching blog: " + err.Error()), http.StatusNotFound
 	}
 
 	blogResponse := make(map[string]utils.BlogResponse)
 	returnMessage := "No Blogs found with these tags"
 
-	if (len(blogs) > 0) {
+	if len(blogs) > 0 {
 		for _, blog := range blogs {
 			blogResponse[blog.Date] = utils.BlogResponse{
 				ID:     blog.ID,
@@ -297,10 +297,10 @@ func HandleGetBlogsByTagIDs(w http.ResponseWriter, r *http.Request) (*utils.GetB
 		returnMessage = "Blogs fetched successfully"
 	}
 
-    return &utils.GetBlogsResponse{
-        Message:    returnMessage,
-        Blogs:      blogResponse,
-        Count:      count,
-        TotalPages: totalPages,
-    }, nil, http.StatusOK
+	return &utils.GetBlogsResponse{
+		Message:    returnMessage,
+		Blogs:      blogResponse,
+		Count:      count,
+		TotalPages: totalPages,
+	}, nil, http.StatusOK
 }
